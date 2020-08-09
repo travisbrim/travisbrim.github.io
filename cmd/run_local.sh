08f84700c67ac272569ebfebc5908b4fba5151f6
Travis Brim <me@travisbrim.com>
2020-08-08 19:22:53 -0500

    rename dir

--- cmd/run_local.sh
+++ cmd/run_local.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-docker run -it -v $(pwd):/app -p 4000:4000 jekyll/builder:pages /bin/bash -c "apk add build-base libxml2-dev libxslt-dev; cd /app/site; jekyll serve"
+docker run -it -v $(pwd):/app -p 4000:4000 jekyll/builder:pages /bin/bash -c "apk add build-base libxml2-dev libxslt-dev; cd /app/docs; jekyll serve"
